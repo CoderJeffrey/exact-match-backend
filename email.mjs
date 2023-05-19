2355ea75a0ddbcae39565ee4ccfbe4b2a8820db5
Jeffrey Liu <jliu5021@usc.edu>
2023-05-19 15:58:49 -0700

    Update for backend server for email sending

--- email.mjs
+++ email.mjs
@@ -7,14 +7,6 @@ dotenv.config();
 import cron from 'node-cron';
 import { createClient } from '@supabase/supabase-js';
 
-// Replace these with your own Supabase and SMTP configurations
-// const SUPABASE_URL = 'https://jgsjaedtspvvamlbginr.supabase.co';
-// const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impnc2phZWR0c3B2dmFtbGJnaW5yIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4MTQ5NTY3NSwiZXhwIjoxOTk3MDcxNjc1fQ.ROK79whMn6Tn0joTHWM9Q694WLOBLisTJFGzjiYWhkc';
-// const EMAILJS_SERVICE_ID = 'service_dktbkwx';
-// const EMAILJS_TEMPLATE_ID = 'template_dqwvwk7';
-// const EMAILJS_PUBLIC_KEY = 'tJkFLj72wE3NEF-VO';
-// const EMAILJS_PRIVATE_KEY = '_eVsxgBkv3x8CEcWtFzA6';
-
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
@@ -26,7 +18,6 @@ emailjs.init({
     publicKey: process.env.EMAILJS_PUBLIC_KEY,
     privateKey: process.env.EMAILJS_PRIVATE_KEY, // optional, highly recommended for security reasons
  });
-
   
 const port = 3000;
 server.listen(port, () => {
@@ -35,10 +26,88 @@ server.listen(port, () => {
   
   // Create a Supabase client
   const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
-  
-  const sendEmail = async (email) => {
-    const message_sent_to_user = `Hello, this is a test email for ${email}`;
+
+  // fetch user info based on the email address in the db Users table
+  const fetch_user_info = async (email) => {
+    const { data: users, error } = await supabase
+      .from('Users')
+      .select('*')
+      .eq('email_address', email)
+      .single();
     
+    if (error) {
+      console.error('Error fetching users:', error);
+      return;
+    }
+    
+    return users;
+  };
+
+  // fetch all jobs from the db Positions table
+  const fetch_all_jobs = async () => {
+    const { data: jobs, error } = await supabase
+      .from('Positions')
+      .select('*');
+    
+    if (error) {
+      console.error('Error fetching jobs:', error);
+      return;
+    }
+
+    return jobs;
+  };
+  
+  const sendEmail = async (email, jobs) => {
+    const message_sent_to_user = `
+      Hello, this is a test email for ${email}
+    `;
+
+    let user_info = await fetch_user_info(email);
+
+    // based on user crtieria
+    // class: Sophomore
+    // race: Asian
+    // job_type: Internship
+    // Job Title: Software Engineer Intern
+    // sponsorship: 'YES'
+
+    // Send out email to user with only the jobs that match their criteria
+    // loop though all jobs and check if it matches the user's criteria
+    // if it does, add it to the message_sent_to_user
+    // if it doesn't, do nothing
+    let filtered_data = jobs.filter((position) => {
+      // print out the job and the criteria sponsorship
+      // console.log("Job.sponsorship == criterias.sponsorship:", position.Sponsorship === user_info.sponsorship)
+      // if the job give sponsorship and the criteria can be either YES OR NO
+      // if the job does not give sponsorship and the criteria must be NO
+      if ((position.Sponsorship === "YES") || (position.Sponsorship=== "NO" && user_info.sponsorship === "NO") || (user_info.sponsorship === null)) {
+        if(position.Diversity.includes(user_info.race) || (user_info.race === null)){
+          if(position.GradeLevel.includes(user_info.class) || (user_info.class === null)){
+            // if the job type is the same as the criteria job type
+            if ((position.Position === user_info.job_title) || (user_info.job_title === null)) {
+              return true;
+            } 
+          }
+        }
+      }
+      return false;
+    });
+
+    // print out the filtered data
+    console.log("filtered_data  length:",filtered_data.length, 
+      " for user sponsorship:", user_info.sponsorship, 
+      " race: ", user_info.race,
+      " class: ", user_info.class,
+      " job_title: ", user_info.job_title
+    );
+    
+    // if the length of the filtered data is over 10, only send the first 10 jobs
+    if (filtered_data.length > 10) {
+      filtered_data = filtered_data.slice(0,10);
+    }
+
+    
+    /*
     try {
     // print out the public key
     // console.log("public key:",EMAILJS_PUBLIC_KEY);
@@ -57,12 +126,35 @@ server.listen(port, () => {
     } catch (error) {
       console.log("error:",error);
     }
+    */
 
     
   };
+
+  const send_emails = async (users) => {
+    // Get all users that want to receive emails
+    // Create an array of promises to send emails concurrently
+    let jobs = await fetch_all_jobs();
+
+    // print out user_info and jobs
+    // console.log('jobs:', jobs);
+
+    const emailPromises = users.map((user) => {
+      sendEmail(user.email_address, jobs)
+      console.log('Email sent to:', user.email_address);
+    });
+
+    try {
+      // Send emails to all users concurrently
+      await Promise.all(emailPromises);
+      console.log('All emails sent');
+    } catch (error) {
+      console.log('Error sending emails:', error);
+    }
+  }
   
   // Set the cron job to run every 15 seconds
-  cron.schedule('*/15 * * * * *', async () => {
+  cron.schedule('*/5 * * * * *', async () => {
     // Get all users that want to receive emails
     const { data: users, error } = await supabase
       .from('Users')
@@ -73,18 +165,7 @@ server.listen(port, () => {
       console.error('Error fetching users:', error);
       return;
     }
-  
-    // Create an array of promises to send emails concurrently
-    const emailPromises = users.map((user) => {
-        sendEmail(user.email_address)
-        console.log('Email sent to:', user.email_address);
-    });
-  
-    try {
-      // Send emails to all users concurrently
-      await Promise.all(emailPromises);
-      console.log('All emails sent');
-    } catch (error) {
-      console.log('Error sending emails:', error);
-    }
+
+    // Send emails to all users concurrently
+    send_emails(users);    
   });
