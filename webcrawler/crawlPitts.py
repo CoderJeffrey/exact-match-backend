0d743695139b9d4d51c0bac26ac4798235f285dc
Alex-YuYan <alex.yan@nyu.edu>
2023-09-18 10:39:17 -0400

    Update pittcsc & simplify crawling scripts

--- webcrawler/crawlPitts.py
+++ webcrawler/crawlPitts.py
@@ -19,69 +19,72 @@ readme_response = requests.get(repo_url + readme_endpoint, headers=headers)
 # Parse the response JSON to get the download URL for the README file
 readme_json = json.loads(readme_response.content)
 readme_download_url = readme_json["download_url"]
+# print(readme_download_url)
 
 # Send a request to download the README file
 readme_response = requests.get(readme_download_url)
+raw_readme = readme_response.text
+# print(raw_readme)
 
-# Parse the HTML content of the README file using BeautifulSoup
-soup = BeautifulSoup(readme_response.content, 'html.parser')
+# find the table of internships
+# | Company | Role | Location | Application/Link | Date Posted |
+# | --- | --- | --- | :---: | :---: |
+table_start = raw_readme.find("| Company | Role | Location | Application/Link | Date Posted |")
+table_end = raw_readme.find("[⬆️ Back to Top ⬆️]")
 
-# print out the text of the README file
-md = soup.get_text()
-
-# from the markdown file, find the table of internships
-# the table header is in the format | Name | Location | Notes |
-table_start = md.find("| Name | Location | Notes |")
-table_end = md.find("[⬆️ Back to Top ⬆️](https://github.com/pittcsc/Summer2023-Internships#the-list-)", table_start)
-
-# extract the table content
-table_content = md[table_start:table_end]
-
-
-# Split the table content into rows
-rows = table_content.split("\n")
-
-# Remove empty rows
+# for each row, split the columns and add to a list of dictionaries
+rows = raw_readme[table_start:table_end].split("\n")
 rows = [row for row in rows if row.strip()]
+rows = rows[2:]
 
-# Split the header row into columns
-headers = rows[0].split("|")
-headers = [header.strip() for header in headers][1:-1]
-headers.append("Sponsorship")
-headers.append("Link")
-
-# Create a list of dictionaries for each row
+# For each in row, split the columns and add to a list of dictionaries split by |
 data = []
-for row in rows[2:]:
+for row in rows:
     values = row.split("|")[1:]
     values = [value.strip() for value in values]
     if len(values) == 0: continue
     name = values[0]
-    link = "null"
-    match = re.search(r"\[(.*?)\]\((.*?)\)", name)
-    print(match)
-    sponsorship = "Yes"
-    if "sponsorship" in values[2].lower() or "citizen" in values[2].lower():
-        sponsorship = "No"
-    if match:
-        name = match.group(1)
-        link = match.group(2)
+    link = values[3]
     data.append({
         "Name": name,
-        "Location": values[1],
-        "Notes": values[2],
-        "Sponsorship": sponsorship,
-        "Link": link
+        "Role": values[1],
+        "Location": values[2],
+        "Link": link,
     })
 
+# For each in data, remove the ** ** around the company name
+for i in range(len(data)):
+    data[i]["Name"] = data[i]["Name"].replace("**", "")
+    # if the name is in markdown link format, remove the []() keep what's in []
+    if data[i]["Name"].find("[") != -1:
+        data[i]["Name"] = data[i]["Name"][data[i]["Name"].find("[")+1 : data[i]["Name"].find("]")]
 
-# Save the data to a JSON file, overwriting the file each time
-with open("scraped_pittcsc.json", "w") as f:
-    json.dump(data, f, indent=4)
-    
+    # the link is going to look like <a href="...">...<img></img></a>, only keep the href part
+    # keep the part between the first href=" and the first ">
+    link = data[i]["Link"]
+    if link == "🔒":
+        # remove this row
+        data[i] = None
+        continue
+    link_start = link.find("href=\"")
+    link_end = link.find("\">")
+    data[i]["Link"] = link[link_start+6 : link_end]
 
+    # Search for 🛂🇺🇸 in the role, if it exists, remove the symbol and add a "sponsorship" field as "no"
+    if data[i]["Role"].find("🛂") != -1 or data[i]["Role"].find("🇺🇸") != -1:
+        data[i]["Sponsorship"] = "No"
+        data[i]["Role"] = data[i]["Role"].replace("🛂", "")
+        data[i]["Role"] = data[i]["Role"].replace("🇺🇸", "")
+    else:
+        data[i]["Sponsorship"] = "Yes"
 
+    # Remove "Role" field and replace with "Notes" field
+    data[i]["Notes"] = data[i]["Role"]
+    data[i].pop("Role")
 
-with open("scraped_pittcsc.json", "w") as f:
-    json.dump(data, f, indent=4)
+# Remove none values
+data = [d for d in data if d is not None]
 
+# Dump the list of jobs into a JSON object
+with open("scraped_pittcsc.json", "w") as outfile:
+    json.dump(data, outfile, indent=4)
